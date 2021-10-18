81bc7d5d5e872feb4f1d5feebe42382e44c32939
xclusive-pc <ya.5164646777752015@yandex.ru>
2021-10-18 12:07:14 +0300

    Add Logger

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,6 +1,8 @@
 <?php
 
 use DI\ContainerBuilder;
+use Donchev\Log\Loggers\FileLogger;
+use Psr\Log\LoggerInterface;
 
 return function (array $settings) {
     $builder = new ContainerBuilder();
@@ -8,6 +10,14 @@ return function (array $settings) {
     if ($settings['env'] === 'prod') {
         $builder->enableCompilation(__DIR__ . '/../cache/container.php');
     }
+
+    $fileLogger = new FileLogger(dirname(__DIR__) . '/logs/application.log');
+
+    $builder->addDefinitions(
+        [
+            LoggerInterface::class => $fileLogger
+        ]
+    );
 
     return $builder->build();
 };
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace Donchev\Framework\Controller;
 
+use Psr\Log\LoggerInterface;
+
 class HomeController
 {
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->info('test');
         var_dump('Hello!');
     }
 }
