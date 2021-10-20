3870c0c9fd76ddba7de9769ccde73bf961ffc91c
xclusive-pc <ya.5164646777752015@yandex.ru>
2021-10-20 17:55:59 +0300

    Cache Clear refactoring

--- src/Command/CacheClearCommand.php
+++ src/Command/CacheClearCommand.php
@@ -24,10 +24,27 @@ class CacheClearCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $path = dirname(__DIR__, 2) . '/var/cache/*';
+        $path = dirname(__DIR__, 2) . '/var/cache';
 
-        shell_exec("sudo rm -rf " . $path);
+        $this->delete($path, $path);
+
+        $output->writeln('<fg=green>==> Cache cleared!</>');
 
         return Command::SUCCESS;
+    }
+
+    private function delete(string $dir, string $rootDir)
+    {
+        foreach (glob($dir . '/*') as $file) {
+            if (is_dir($file)) {
+                $this->delete($file, $rootDir);
+            } else {
+                unlink($file);
+            }
+        }
+
+        if ($dir != $rootDir) {
+            rmdir($dir);
+        }
     }
 }
