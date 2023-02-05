2c35fff801bb6a256753b5f0b65af18878cf23c5
xclusive-pc <ya.5164646777752015@yandex.ru>
2023-02-05 06:19:31 +0200

    Add token generator

---
+++ src/Security/TokenGenerator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Donchev\Framework\Security;
+
+use Exception;
+
+class TokenGenerator
+{
+    /**
+     * @throws Exception
+     */
+    public static function generate(int $length = 32): string
+    {
+        return bin2hex(random_bytes($length));
+    }
+}
