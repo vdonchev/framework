8b15d7eba37b0cd84d98f45aaf6007021ad168a1
xclusive-pc <ya.5164646777752015@yandex.ru>
2023-03-08 21:59:50 +0200

    Update Get and Post classes

--- src/Http/Get.php
+++ src/Http/Get.php
@@ -4,19 +4,42 @@ namespace Donchev\Framework\Http;
 
 class Get
 {
-    public static function getAll()
+    public static function getAll(bool $trim = true)
     {
+        if ($trim === true) {
+            return filter_var($_GET, FILTER_CALLBACK, ['options' => 'trim']);
+        }
+
         return $_GET;
     }
 
-    public static function get(string $key)
+    public static function get(string $key, bool $trim = true)
     {
-        return $_GET[$key] ?? null;
+        if (isset($_GET[$key])) {
+            if ($trim === true) {
+                return filter_var($_GET[$key], FILTER_CALLBACK, ['options' => 'trim']);
+            }
+
+            return $_GET[$key];
+        }
+
+        return null;
     }
 
     public static function exist(string $key): bool
     {
         return isset($_GET[$key]);
+    }
+
+    public static function exists(array $keys): bool
+    {
+        foreach ($keys as $key) {
+            if (!isset($_GET[$key])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static function empty(string $key): bool
--- src/Http/Post.php
+++ src/Http/Post.php
@@ -2,21 +2,46 @@
 
 namespace Donchev\Framework\Http;
 
+use const FILTER_CALLBACK;
+
 class Post
 {
-    public static function getAll()
+    public static function getAll(bool $trim = true)
     {
+        if ($trim === true) {
+            return filter_var($_POST, FILTER_CALLBACK, ['options' => 'trim']);
+        }
+
         return $_POST;
     }
 
-    public static function get(string $key)
+    public static function get(string $key, bool $trim = true)
     {
-        return $_POST[$key] ?? null;
+        if (isset($_POST[$key])) {
+            if ($trim === true) {
+                return filter_var($_POST[$key], FILTER_CALLBACK, ['options' => 'trim']);
+            }
+
+            return $_POST[$key];
+        }
+
+        return null;
     }
 
     public static function exist(string $key): bool
     {
         return isset($_POST[$key]);
+    }
+
+    public static function exists(array $keys): bool
+    {
+        foreach ($keys as $key) {
+            if (!isset($_POST[$key])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static function empty(string $key): bool
